e5b3951584b2f8305940f1873f62b685b38933fe
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-10-30 20:43:36 -0700

    Add project files.

---
+++ IanProgrammerTestSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IanProgrammerTestSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
